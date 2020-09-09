0c9dc04cf71d24030cbf42352ee76b99677be071
devlloplugin <70232551+devlloplugin@users.noreply.github.com>
2020-09-09 22:24:14 +0200

    Create devllo-events-pmpro.php

---
+++ devllo-events-pmpro.php
@@ -0,0 +1,11 @@
+<?php
+/*
+    Plugin Name: PMPro Integration for Devllo Events
+    Plugin URI: https://devlloplugins.com/
+    Description: This adds an integration with PMPro to restrict events to PMPro members
+    Author: Devllo Plugins
+    Version: 0.0.1
+    Author URI: https://devllo.com/
+    Text Domain: devllo-events-pmpro
+    Domain Path: /languages
+ */
