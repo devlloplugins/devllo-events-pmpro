91281cf77ee9e9b0c2b338262bcfae765ad0347a
Femi <94sam.fem@gmail.com>
2020-09-17 23:25:49 +0200

    Add amin notices
    Add admin notices if PMPro or Devllo Events is not activated

--- devllo-events-pmpro.php
+++ devllo-events-pmpro.php
@@ -10,67 +10,77 @@
     Domain Path: /languages
  */
 
-add_action( 'wp_enqueue_scripts', 'devllo_events_pmpro_enqueue_styles' );
-require_once plugin_dir_path( __FILE__ ) . 'inc/devllo-events-pmpro-admin.php';
 
-function devllo_events_pmpro_enqueue_styles(){
-wp_enqueue_style( 'devllo-events-pmpro-styles', plugin_dir_url( __FILE__ ) . 'assets/css/style.css');	
+if ( (in_array( 'devllo-events/devllo-events.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) )) &&  (in_array( 'paid-memberships-pro/paid-memberships-pro.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) )) ) {
+
+    add_action( 'wp_enqueue_scripts', 'devllo_events_pmpro_enqueue_styles' );
+    require_once plugin_dir_path( __FILE__ ) . 'inc/devllo-events-pmpro-admin.php';
+
+    function devllo_events_pmpro_enqueue_styles(){
+    wp_enqueue_style( 'devllo-events-pmpro-styles', plugin_dir_url( __FILE__ ) . 'assets/css/style.css');	
+    }
+
+
+    function devllo_events_pmpro_page_meta_wrapper( ) {
+        if ( defined( 'PMPRO_VERSION' ) ) {
+            add_meta_box( 'pmpro_page_meta', 'Require Membership', 'pmpro_page_meta', 'devllo_event', 'side', 'high' );
+        }
+    }
+    add_action( 'admin_menu', 'devllo_events_pmpro_page_meta_wrapper' );
+
+
+
+
+    function devllo_events_pmpro_has_membership_access( $hasaccess, $post, $user, $levels ) {
+        
+        if ( $post->post_type == 'devllo_event' && ! $hasaccess ) {
+            // Do something here if user has no access
+
+
+        }
+        return $hasaccess;
+
+    }
+    add_filter( 'pmpro_has_membership_access_filter', 'devllo_events_pmpro_has_membership_access', 10, 4 );
+
+    function devllo_events_show_details(){
+    global $post;
+
+        $id = get_the_ID();
+
+    //  $map_location = get_post_meta( $post->ID, 'devllo_event_location_key', true );
+
+
+        $show_event_website = get_post_meta($id, 'devllo_show_event_website_key', true);
+        $show_event_online_link = get_post_meta($id, 'devllo_show_event_online_link_key', true);
+        $show_event_location = get_post_meta($id, 'devllo_show_event_location_key', true);
+        $show_event_map = get_post_meta($id, 'devllo_show_event_map_key', true);
+
+        if ($show_event_website)
+        {
+            add_filter('event_website_content_filter', '__return_false' );
+        }
+
+        if ($show_event_online_link)
+        {
+            add_filter('event_online_link_content_filter', '__return_false' );
+        }
+
+        if ($show_event_location)
+        {
+            add_filter('event_location_name_content_filter', '__return_false' );
+        }
+
+        if ($show_event_map)
+        {
+            add_filter('event_map_location_content_filter', '__return_false' );
+        }
+    }
+    add_action( 'wp', 'devllo_events_show_details' ); 
+}else
+{
+    function devllo_events_pmpro_admin_notice(){
+    echo '<div class="notice notice-error is-dismissible"><p> The PMPro integration add on for Devllo Events is installed but the Devllo Events plugin and/or the PMPro plugin is not installed</p></div>';
+    }
+    add_action('admin_notices', 'devllo_events_pmpro_admin_notice');
 }
-
-
-function devllo_events_pmpro_page_meta_wrapper( ) {
-	if ( defined( 'PMPRO_VERSION' ) ) {
-		add_meta_box( 'pmpro_page_meta', 'Require Membership', 'pmpro_page_meta', 'devllo_event', 'side', 'high' );
-	}
-}
-add_action( 'admin_menu', 'devllo_events_pmpro_page_meta_wrapper' );
-
-
-
-
-function devllo_events_pmpro_has_membership_access( $hasaccess, $post, $user, $levels ) {
-	
-	if ( $post->post_type == 'devllo_event' && ! $hasaccess ) {
-        // Do something here if user has no access
-
-
-    }
-    return $hasaccess;
-
-}
-add_filter( 'pmpro_has_membership_access_filter', 'devllo_events_pmpro_has_membership_access', 10, 4 );
-
-function devllo_events_show_details(){
-global $post;
-
-    $id = get_the_ID();
-
-  //  $map_location = get_post_meta( $post->ID, 'devllo_event_location_key', true );
-
-
-    $show_event_website = get_post_meta($id, 'devllo_show_event_website_key', true);
-    $show_event_online_link = get_post_meta($id, 'devllo_show_event_online_link_key', true);
-    $show_event_location = get_post_meta($id, 'devllo_show_event_location_key', true);
-    $show_event_map = get_post_meta($id, 'devllo_show_event_map_key', true);
-
-    if ($show_event_website)
-    {
-        add_filter('event_website_content_filter', '__return_false' );
-    }
-
-    if ($show_event_online_link)
-    {
-        add_filter('event_online_link_content_filter', '__return_false' );
-    }
-
-    if ($show_event_location)
-    {
-        add_filter('event_location_name_content_filter', '__return_false' );
-    }
-
-    if ($show_event_map)
-    {
-        add_filter('event_map_location_content_filter', '__return_false' );
-    }
-}
-add_action( 'wp', 'devllo_events_show_details' ); 
